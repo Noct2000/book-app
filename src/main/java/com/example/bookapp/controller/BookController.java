4d7088a6a670fee92d2e58226ef2d30ce83c2b01
Noct2000 <oleg.kvasha.00@gmail.com>
2024-01-31 17:13:04 +0200

    remove swagger

--- src/main/java/com/example/bookapp/controller/BookController.java
+++ src/main/java/com/example/bookapp/controller/BookController.java
@@ -3,7 +3,6 @@ package com.example.bookapp.controller;
 import com.example.bookapp.dto.request.BookRequestDto;
 import com.example.bookapp.dto.response.BookResponseDto;
 import com.example.bookapp.service.BookService;
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -23,10 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
     private final BookService bookService;
 
-    @Operation(
-            summary = "Create new book",
-            description = "Create new book from given JSON"
-    )
     @PostMapping
     public ResponseEntity<BookResponseDto> save(
             @RequestBody
@@ -36,28 +31,16 @@ public class BookController {
         return ResponseEntity.ok(bookService.save(bookRequestDto));
     }
 
-    @Operation(
-            summary = "Get book by id",
-            description = "Get book by id. If book is not present then error will be thrown"
-    )
     @GetMapping("/{id}")
     public ResponseEntity<BookResponseDto> getById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getById(id));
     }
 
-    @Operation(
-            summary = "Get all books",
-            description = "Get list of all books"
-    )
     @GetMapping
     public ResponseEntity<List<BookResponseDto>> getAll() {
         return ResponseEntity.ok(bookService.getAll());
     }
 
-    @Operation(
-            summary = "Update book",
-            description = "Update book if book is present"
-    )
     @PutMapping("/{id}")
     public ResponseEntity<BookResponseDto> update(
             @PathVariable
@@ -69,10 +52,6 @@ public class BookController {
         return ResponseEntity.ok(bookService.update(id, bookRequestDto));
     }
 
-    @Operation(
-            summary = "Delete book",
-            description = "Delete book by id"
-    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         bookService.deleteById(id);
